12db73a46aef79cc68f506314fb0d0040c902d87
Blackknight333 <franalan333@hotmail.com>
2026-01-15 00:41:27 +0100

    add lists to webpage

--- webshit/app.js
+++ webshit/app.js
@@ -3,13 +3,25 @@ let signer;
 let contract;
 let balance;
 
+
 const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 const abi = [
   "function transfer(address to, uint256 value) returns (bool)",
   "function balanceOf(address owner) view returns (uint256)"
 ];
 
+let lista = ""
+for(let i = 0; i<accounts.length; i++){
+  lista += `<option value="${i}">${accounts[i].name}</option>`
+}
+console.log(lista)
+document.getElementById("accounts").innerHTML = lista;
+document.getElementById("to").innerHTML = lista;
+let value_from_lista;
+
 async function connectWallet() {
+  console.log("CALLED connectWallet")
+  console.log(accounts[0].name)
   /*if (!window.ethereum) {
     alert("Instala MetaMask");
     return;
@@ -18,7 +30,9 @@ async function connectWallet() {
   //provider = new ethers.BrowserProvider(window.ethereum);
   provider = new ethers.JsonRpcProvider();
   // await provider.send("eth_requestAccounts", []);
-  signer = await provider.getSigner();
+  value_from_lista = document.getElementById("accounts").value
+  console.log(accounts[value_from_lista].address)
+  signer = await provider.getSigner(accounts[value_from_lista].address);
 
 
 
@@ -27,12 +41,15 @@ async function connectWallet() {
 
   contract = new ethers.Contract(contractAddress, abi, signer);
   balance = await contract.balanceOf(address);
-  console.log(balance)
+  console.log("BALANCE: " + balance)
   document.getElementById("balance").innerText = `Pugies: ${balance}`
 }
 
 async function sendTokens() {
-  const to = document.getElementById("to").value;
+  let index = document.getElementById("to").value
+  console.log(index)
+  let to = accounts[index].address
+  //const to = document.getElementById("to").value;
   console.log(to)
   const amount = document.getElementById("amount").value;
   console.log(amount)
